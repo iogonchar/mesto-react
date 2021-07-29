e0e2d6c7d7385f1dda1d5147c83ce2466a159fe9
Ilya Gonchar <i.o.gonchar@yandex.ru>
2021-07-29 12:18:30 +0300

    refactor: Footer component added

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,14 @@
 // component imports
 import Header from './Header';
 import Main from './Main';
+import Footer from './Footer';
 
 function App() {
   return (
     <div className="page__content">
       <Header />
       <Main />
-
-
-      <footer className="footer">
-        <p className="footer__copyright" lang="en">&copy; 2021 Mesto Russia</p>
-      </footer>
+      <Footer />
     </div>
   );
 }
