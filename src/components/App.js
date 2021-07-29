179b8c9b5049fddbc9d75ead9355b8f755496b78
Ilya Gonchar <i.o.gonchar@yandex.ru>
2021-07-29 12:16:44 +0300

    refactor: Main component added

--- src/components/App.js
+++ src/components/App.js
@@ -1,60 +1,13 @@
 // component imports
 import Header from './Header';
-
-// image imports
-import imgEditProfile from '../images/edit-profile.svg';
-import imgAddCard from '../images/add-button.svg';
-
-// Аватар временно !!!
-import imgAvatar from '../images/avatar.jpg';
-
-// import './App.css';
+import Main from './Main';
 
 function App() {
   return (
     <div className="page__content">
       <Header />
+      <Main />
 
-      <main className="content">
-        <section className="profile">
-          {/* avatar */}
-          <div className="profile__avatar-wrapper">
-            <img
-              className="profile__avatar"
-              src={imgAvatar}
-              alt="Аватар"
-            />
-            <button className="profile__avatar-change-btn"></button>
-          </div>
-
-          {/* user info */}
-          <div className="profile__info-wrapper">
-            <div className="profile__info">
-              <h1 className="profile__author">Жак-Ив Кусто</h1>
-              <p className="profile__about">Исследователь океана</p>
-            </div>
-
-            <button className="profile__profile-edit-btn">
-              <img
-                className="profile__profile-edit"
-                src={imgEditProfile}
-                alt="Редактировать профиль"
-              />
-            </button>
-          </div>
-
-          {/* add new card */}
-          <button className="profile__add-card-btn">
-            <img
-              className="profile__add-card"
-              src={imgAddCard}
-              alt="Добавить новое место"
-            />
-          </button>
-        </section>
-
-        <section className="places"></section>
-      </main>
 
       <footer className="footer">
         <p className="footer__copyright" lang="en">&copy; 2021 Mesto Russia</p>
---
+++ src/components/Main.js
@@ -0,0 +1,52 @@
+import imgEditProfile from '../images/edit-profile.svg';
+import imgAddCard from '../images/add-button.svg';
+
+// Аватар временно !!!
+import imgAvatar from '../images/avatar.jpg';
+
+function Main() {
+  return (
+    <main className="content">
+      <section className="profile">
+        {/* avatar */}
+        <div className="profile__avatar-wrapper">
+          <img
+            className="profile__avatar"
+            src={imgAvatar}
+            alt="Аватар"
+          />
+          <button className="profile__avatar-change-btn"></button>
+        </div>
+
+        {/* user info */}
+        <div className="profile__info-wrapper">
+          <div className="profile__info">
+            <h1 className="profile__author">Жак-Ив Кусто</h1>
+            <p className="profile__about">Исследователь океана</p>
+          </div>
+
+          <button className="profile__profile-edit-btn">
+            <img
+              className="profile__profile-edit"
+              src={imgEditProfile}
+              alt="Редактировать профиль"
+            />
+          </button>
+        </div>
+
+        {/* add new card */}
+        <button className="profile__add-card-btn">
+          <img
+            className="profile__add-card"
+            src={imgAddCard}
+            alt="Добавить новое место"
+          />
+        </button>
+      </section>
+
+      <section className="places"></section>
+    </main>
+  );
+}
+
+export default Main;
